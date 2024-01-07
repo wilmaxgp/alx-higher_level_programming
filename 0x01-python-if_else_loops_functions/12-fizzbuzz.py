923b5988563dcd08dce10e09dd41e56e8b033333
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-01-06 20:52:27 -0800

    fix code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
@@ -11,6 +10,7 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
+
 
 if __name__ == "__main__":
     fizzbuzz()
