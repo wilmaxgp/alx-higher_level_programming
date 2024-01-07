f3c6a9cf0b42ef51cbcf4d265525c9315eebfb10
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-01-06 19:25:36 -0800

    fix code

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+"""
+The output of the program should be:
+The string Last digit of, followed by
+the number, followed by
+the string is, followed by the last digit of number, followed by
+if the last digit is greater than 5: the string and is greater than 5
+if the last digit is 0: the string and is 0
+if the last digit is less than 6 and not 0: the string and is less than 6 and not 0
+followed by a new line
+"""
 last_digt = abs(number) % 10
-if number < 0:
-    last_digt *= -1
-print(f"Last digt of {number:d} is {last_digt:d} and is ", end="")
-if last_digt > 5:
-    print("greater than 5")
-if last_digt == 0:
-    print("0")
+
+if number == 0:
+    last_digt = 0
+elif number > 0:
+    last_digt = last_digt
 else:
-    print("less than 6 and not 0")
+    last_digt = -1
+
+if last_digt > 5:
+    strg = "the string and is greater than 5"
+elif last_digt == 0:
+    strg = "the string and is 0"
+elif last_digt < 6 and last_digt != 0:
+    strg = "the string and is less than 6 and not 0"
+
+print(f"Last digit of {number:d} is {last_digt} {strg}") 
