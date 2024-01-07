b7c5908e5e447821497c4fa898154c75934d09da
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-01-06 21:18:58 -0800

    add integers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-from add_0 import add
+    from add_0 import add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
