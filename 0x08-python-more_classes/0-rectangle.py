c6ebfecaec1e6c2e4da6fe7df4df90dc77d0db24
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-02-17 04:49:43 -0800

    fix errors

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 class Rectangle:
+    """
+    Empty class representing a rectangle.
+    """
     pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
