9b7a7a68acdf284dee47467e247a977610c85190
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-14 09:50:29 +0100

    Fixed SQL queries to accurately reflect the structure and data of the hbtn_0d_tvshows database dump after downloading the file.

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,6 @@
 -- SQL script to list all cities contained in the database hbtn_0d_usa by ascending order
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
