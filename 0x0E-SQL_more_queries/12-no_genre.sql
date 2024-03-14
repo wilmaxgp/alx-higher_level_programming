739418066cdd111a3f0c505e9ff74446e0a3c981
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-14 09:53:51 +0100

    Fixed SQL queries to accurately reflect the structure and data of the hbtn_0d_tvshows database dump after downloading the file.

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,4 +3,5 @@
 SELECT tv_shows.title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL;
+GROUP BY tv_shows.title
+HAVING COUNT(tv_show_genres.genre_id) = 0;
