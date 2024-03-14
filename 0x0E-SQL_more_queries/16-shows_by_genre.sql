c29504aa5bce331cb7afb7d42be8a4b8e5ef1654
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-14 12:39:21 +0100

    Fixed SQL queries to accurately reflect the structure and data of the hbtn_0d_tvshows database dump after downloading the file.

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- SQL script to add a city to the cities table named `Palo Alto` in California
 
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
