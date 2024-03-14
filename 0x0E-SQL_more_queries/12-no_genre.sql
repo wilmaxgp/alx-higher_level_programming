613008111bb12ce1676096522a4dd58e8026d057
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-14 09:52:23 +0100

    Fixed SQL queries to accurately reflect the structure and data of the hbtn_0d_tvshows database dump after downloading the file.

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,6 @@
 -- SQL script to change the name California to Cali on the states table
-UPDATE states SET name = 'Cali' WHERE name = 'California';
+
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL;
