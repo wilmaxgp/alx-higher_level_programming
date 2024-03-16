427105d3ad10d4fc12db59c25531265b720f8ee1
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-16 12:24:19 +0100

    Updated SQL script

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,23 +1,8 @@
 -- SQL script to print full description of table first_table from database hbtn_0c_0
 
-SELECT CONCAT(
-    'CREATE TABLE `', TABLE_NAME, '` (',
-    GROUP_CONCAT(
-        CONCAT(
-            '`', COLUMN_NAME, '` ',
-            COLUMN_TYPE,
-            IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
-            IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
-            IF(COLUMN_KEY = 'PRI', ' PRIMARY KEY', ''),
-            IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', '')
-        )
-        ORDER BY ORDINAL_POSITION
-        SEPARATOR ', '
-    ),
-    ') ENGINE=', ENGINE,
-    ' DEFAULT CHARSET=', CHARACTER_SET_NAME, 
-    ' COLLATE=', COLLATION_NAME
-) AS full_description
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SET @db_name = 'hbtn_0c_0';
+SET @table_name = 'first_table';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA, COLUMN_COMMENT
+FROM information_schema.columns
+WHERE table_schema = @db_name
+AND table_name = @table_name;
