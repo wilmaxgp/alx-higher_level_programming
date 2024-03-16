9caeb9779d2e76c920447fd4869a5d7afb68f34d
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-16 12:42:41 +0100

    fixing of script output

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,10 +1,12 @@
 -- SQL script to print full description of table first_table from database hbtn_0c_0
 
-DROP TABLE IF EXISTS `first_table`;
-CREATE TABLE `first_table` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) DEFAULT NULL,
-  `c` char(1) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
