13f0b3c73bd7b09f97ef13689046f14697e423af
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-16 12:09:02 +0100

    Updated SQL script

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 -- SQL script to print full description of table first_table from database hbtn_0c_0
 
-SELECT CONCAT(TABLE_NAME, CREATE_TABLE) AS first_table
+SELECT CONCAT(TABLE_NAME, 'CREATE TABLE ', CREATE_TABLE) AS first_table
 FROM information_schema.TABLES
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
 AND TABLE_NAME = 'first_table';
