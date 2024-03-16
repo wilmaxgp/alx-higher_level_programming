9f1271278dae8df6a041d177d6ef000043170d1b
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-03-16 12:33:07 +0100

    fixing of script output

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,8 +1,9 @@
 -- SQL script to print full description of table first_table from database hbtn_0c_0
 
-SET @db_name = 'hbtn_0c_0';
-SET @table_name = 'first_table';
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA, COLUMN_COMMENT
-FROM information_schema.columns
-WHERE table_schema = @db_name
-AND table_name = @table_name;
+first_tableCREATE TABLE `first_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `c` char(1) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
