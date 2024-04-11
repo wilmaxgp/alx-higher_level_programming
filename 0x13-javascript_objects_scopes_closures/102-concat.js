b0e28f0e4988291ac05deff4d12f5486b85b6c3c
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-11 17:45:01 +0100

    fix outputs

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,8 @@
 
 const fs = require('fs');
 
-// Check if the correct number of arguments are provided
 if (process.argv.length !== 5) {
-  console.log('Usage: ./102-concat.js <file1> <file2> <destination>');
+  console.log('Usage: ./concat.js <file1> <file2> <destination>');
   process.exit(1);
 }
 
@@ -12,17 +11,16 @@ const file1Path = process.argv[2];
 const file2Path = process.argv[3];
 const destinationPath = process.argv[4];
 
-// Read the contents of the first file
-const file1Content = fs.readFileSync(file1Path, 'utf8');
+try {
+  const file1Content = fs.readFileSync(file1Path, 'utf8');
+  const file2Content = fs.readFileSync(file2Path, 'utf8');
 
-// Read the contents of the second file
-const file2Content = fs.readFileSync(file2Path, 'utf8');
+  const concatenatedContent = file1Content + file2Content;
 
-// Concatenate the contents of the two files
-const concatenatedContent = file1Content + file2Content;
+  fs.writeFileSync(destinationPath, concatenatedContent);
 
-// Write the concatenated content to the destination file
-fs.writeFileSync(destinationPath, concatenatedContent);
-
-console.log(`Files ${file1Path} and ${file2Path} concatenated successfully to ${destinationPath}`);
+  console.log('Concatenation successful!');
+} catch (error) {
+  console.error('Error:', error.message);
+}
 
