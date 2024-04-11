a089df2fc3f0f389364d8b06ce150ea34848457a
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-11 15:17:02 +0100

    fix error_4

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const num = parseInt(process.argv[2]);
 
-if (isNaN(num) || num < 0) {
+if (!Number.isInteger(num) || num < 0) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < num; i++) {
