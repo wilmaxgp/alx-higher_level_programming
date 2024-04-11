72d80f039fd19163047086291cf96869051371ab
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-11 15:42:13 +0100

    fix error_7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const num = parseInt(process.argv[2]);
-
-if (isNaN(num) || num <= 0) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const absNum = Math.abs(num); // Convert negative number to its absolute value
-  for (let i = 0; i < absNum; i++) {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
-
