66d88bd3879187324236ff20d12bce7ac70f7341
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-11 17:50:59 +0100

    fix outputs

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,19 +2,12 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 5) {
-  console.log('Usage: ./concat.js <file1> <file2> <destination>');
-  process.exit(1);
-}
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const destination = process.argv[4];
 
-const [,, file1, file2, destination] = process.argv;
+const content1 = fs.readFileSync(file1).toString();
+const content2 = fs.readFileSync(file2).toString();
 
-try {
-  const content1 = fs.readFileSync(file1, 'utf-8');
-  const content2 = fs.readFileSync(file2, 'utf-8');
-  fs.writeFileSync(destination, content1 + content2);
-  console.log('Concatenation successful!');
-} catch (err) {
-  console.error('Error:', err.message);
-}
+fs.writeFileSync(destination, content1 + content2);
 
