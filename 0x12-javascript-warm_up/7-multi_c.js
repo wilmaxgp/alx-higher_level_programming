60ebc97d4db26dddde316ccb47335ba38099569e
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-11 16:40:53 +0100

    Fixed output error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,19 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+
+const numString = process.argv[2];
+
+if (!numString || isNaN(numString)) {
   console.log('Missing number of occurrences');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
+  const num = parseInt(numString);
+  if (num <= 0) {
+    console.log('Missing number of occurrences');
+  } else {
+    let i = 0;
+    while (i < num) {
+      console.log('C is fun');
+      i++;
+    }
   }
 }
+
