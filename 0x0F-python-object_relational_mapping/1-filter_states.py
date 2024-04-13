af4fd86b6feaf7603a40c540f00a43e00ebbffce
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-04-14 00:16:21 +0100

    fix of select states and wrote script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa
+Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
 """
 
 import sys
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cursor.fetchall()
     for state in states:
         print(state)
